d22329622fc65603ebff50eba198ad61849de030
Nguyen Minh Chien <nmchien116@gmail.com>
2025-04-16 17:05:22 +0700

    add joi validate

--- src/apis/users/user.route.js
+++ src/apis/users/user.route.js
@@ -1,19 +1,4 @@
-// import UserController  from "./user.controller.js";
-// import validateMiddleware from "../../middleware/validate.middleware.js";
-// import express from 'express'
-
-// const route = express.Router()
-
-// route.route('/')
-//     .get(UserController.getAllUsers)
-//     .post(UserController.postUser)
-
-// route.route('/:id')
-//     .put(UserController.updateUser)
-//     .get(UserController.getDetailUser)
-//     .delete(UserController.deleteUser)
-
-// export default route
+import validateMiddleware from '../../middleware/validate.middleware.js'
 
 import UserController from './user.controller.js'
 import express from 'express'
@@ -22,10 +7,10 @@ const route = express.Router()
 
 route.route('/')
     .get(UserController.getAllUsers)
-    .post(UserController.postUser)
+    .post(validateMiddleware.validateUser ,UserController.postUser)
 
 route.route('/:id')
-    .get(UserController.getUser)
-    .put(UserController.updateUser)
-    .delete(UserController.deleteUser)
+    .get(validateMiddleware.validateId,UserController.getUser)
+    .put(validateMiddleware.validateId,validateMiddleware.validateUser ,UserController.updateUser)
+    .delete(validateMiddleware.validateId,UserController.deleteUser)
 export default route
